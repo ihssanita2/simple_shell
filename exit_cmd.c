8ac020a0a6eb23c44010093d2d977e9d862d5d10
JalalErr <jalil.errajy@gmail.com>
2023-08-23 03:01:32 -0700

    Editing exit_cmd.c

--- exit_cmd.c
+++ exit_cmd.c
@@ -5,24 +5,24 @@
 /* function 1 : _exit_cmd */
 
 /**
- * _exit_cmd - get out of programme. 
+ * _exit_cmd - get out of programme.
  * @s1: first string.
  * @s2: second string.
  * Return: 0 if s1 and s2 are equals,
  * another number if not.
  */
 
-    int  _exit_cmd(char *s1, char *s2)
-    {
-        int i = 0, v = 0;
+int  _exit_cmd(char *s1, char *s2)
+{
+	int i = 0, v = 0;
 
-        while (v == 0)
-        {
-            if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
-                break;
-            v = *(s1 + i) - *(s2 + i);
-            i++;
-        }
-        return (v);
-    }
-    
+	while (v == 0)
+	{
+		if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
+			break;
+		v = *(s1 + i) - *(s2 + i);
+		i++;
+	}
+	return (v);
+}
+
